d59821c134a9d2454f49ef9006fd5df5d8931e95
arz <arz@weblock.su>
2025-02-18 14:02:25 +0300

    db schema update

--- db/r100.sql
+++ db/r100.sql
@@ -1,7 +1,11 @@
 CREATE TABLE sd_user (
     id bigint NOT NULL PRIMARY KEY,
     title varchar(200) NOT NULL,   
-    file_limit int NOT NULL DEFAULT 0
+    file_limit int NOT NULL DEFAULT 0,
+    wins int NOT NULL DEFAULT 0,
+    half_wins int NOT NULL DEFAULT 0,
+    losses int NOT NULL DEFAULT 0,
+    competitions int NOT NULL DEFAULT 0
 );
 
 CREATE TABLE chat (
